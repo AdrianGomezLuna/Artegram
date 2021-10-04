0ee8eda14ec6b1717b49975bd228d7455b3194bd
AdrianGomezLuna <adrigoluna@gmail.com>
2021-10-04 11:09:51 +0200

    Components Module y Componentes Posts y Post

---
+++ src/app/interfaces/interfaces.ts
@@ -0,0 +1,22 @@
+export interface RespuestaPosts  {
+  ok: boolean;
+  pagina: number;
+  posts: Post[];
+}
+
+// Las interrogaciones es porque es opcional
+export interface Post {
+  _id?: string;
+  mensaje?: string;
+  img?: string[];
+  coords?: string;
+  usuario?: Usuario;
+  created?: string;
+}
+
+export interface Usuario {
+  _id?: string;
+  nombre?: string;
+  avatar?: string;
+  email?: string;
+}

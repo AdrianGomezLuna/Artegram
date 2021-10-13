079d37484b3e82fab25ffffafeeed3b90eda2901
AdrianGomezLuna <adrigoluna@gmail.com>
2021-10-13 17:59:20 +0200

    infinite scroll tab1

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 import { Post } from 'src/app/interfaces/interfaces';
 import { PostsService } from 'src/app/services/posts.service';
 
@@ -9,15 +10,35 @@ import { PostsService } from 'src/app/services/posts.service';
 })
 export class Tab1Page implements OnInit {
 
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
   posts: Post[] = [];
 
   constructor( private postService: PostsService) {}
 
   ngOnInit() {
+    this.siguiente();
+  }
+
+  siguiente( event? ) {
     this.postService.getPosts().subscribe( resp => {
       console.log( resp );
       this.posts.push(...resp.posts); // cada entrada lo trata como un elemento nuevo
+
+      //Controla el infinite Scroll
+      if (event) {
+        setTimeout(()=> {
+          console.log('Done');
+          event.target.complete();
+          if (resp.posts.length === 0) {
+            event.target.disabled = true;
+          }
+        }, 500);
+      }
     });
+  }
+
+  toggleInfiniteScroll() {
+    this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
   }
 
 }

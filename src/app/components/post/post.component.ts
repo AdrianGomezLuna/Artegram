6ed2123990092cb28c51eea635e43827516bc799
AdrianGomezLuna <adrigoluna@gmail.com>
2021-10-13 17:29:16 +0200

    Actualización Post Component html

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 import { Post } from '../../interfaces/interfaces';
 
 @Component({
@@ -10,7 +11,19 @@ export class PostComponent implements OnInit {
 
   @Input() post: Post = {};
 
+  //Mantener los tamaños de las imágenes cuando se cambia la orientación móvil
+  @ViewChild(IonSlides) slides: IonSlides;
+
+  imag1 = '/assets/perro-1.jpg';
+  imag2 = '/assets/perro-2.jpg';
+  imag3 = '/assets/perro-3.jpg';
+
   constructor() { }
+
+  //Mantener los tamaños de las imágenes cuando se cambia la orientación móvil
+  @HostListener('window:resize') onResize(){
+    setTimeout(() => this.slides.update(), 100);
+  }
 
   ngOnInit() {}
 

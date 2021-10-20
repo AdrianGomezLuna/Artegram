f411463f9ef59d6c057e9ae20ed99c16dc85e96d
AdrianGomezLuna <adrigoluna@gmail.com>
2021-10-20 12:27:35 +0200

    Crear mensajes Post y guardar en BD

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PostsService } from '../../services/posts.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  tempImages = [];
+
+  post = {
+    mensaje: '',
+    coords: null,
+    posicion: false
+  };
+
+  constructor( private postService: PostsService) {}
+
+  crearPost(){
+    console.log(this.post);
+    this.postService.crearPost(this.post);
+  }
+
 
 }
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { RespuestaPosts } from '../interfaces/interfaces';
+import { UsuarioService } from './usuario.service';
 
 const URL = environment.url;
 
@@ -12,7 +13,7 @@ export class PostsService {
 
   paginaPosts = 0;
 
-  constructor( private http: HttpClient) { }
+  constructor( private http: HttpClient, private usuarioService: UsuarioService) { }
 
   getPosts(nuevo: boolean = false) {
     if (nuevo) {
@@ -21,5 +22,15 @@ export class PostsService {
     this.paginaPosts++;
     return this.http.get<RespuestaPosts>(`${URL}/posts/?pagina=${this.paginaPosts}`);
     //La URL lo coge de los enviroments
+  }
+
+  crearPost( post) {
+    const headers = new HttpHeaders({
+      'x-token': this.usuarioService.token
+    });
+
+    this.http.post(`${URL}/posts`,post,{headers}).subscribe( resp => {
+      console.log(resp);
+    });
   }
 }
